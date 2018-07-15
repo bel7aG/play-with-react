37f939501dd90ae1c8b9dd1ad649f3ef96c0be47
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-16 01:26:21 +0200

    play play baby

--- src/App.js
+++ src/App.js
@@ -6,25 +6,34 @@ class App extends Component {
     super(props);
     this.state = {
       persons: [
-        {name: 'Belhassen', age: '21'},
-        {name: 'idiot', age: '22'},
-        {name: 'dump', age: '25'}
+        {name: 'bel7aG', age: '21'},
+        {name: 'belhassen', age: '22'},
+        {name: 'DABEBA', age: '25'}
       ]
     }
-
     this.handlePersonName = this.handlePersonName.bind(this);
   }
 
-  handlePersonName() {
+  handlePersonName(newName) {
     this.setState(() => ({
       persons: [
-        {name: 'Belhassen', age: '21', job: 'developer'},
+        {name: newName, age: '21'},
         {name: 'idiot', age: '22'},
         {name: 'dump', age: '25'}
       ]
     }));
   }
 
+  onChangeName = (event) => {
+    const name = event.target.value;
+    this.setState(() => ({
+      persons: [
+        {name: name, age: '21'},
+        {name: 'idiot', age: '22'},
+        {name: 'dump', age: '25'}
+      ]
+    }));
+  };
 
   render() {
     return (
@@ -32,15 +41,13 @@ class App extends Component {
         <h1>Play With ReactLove</h1>
         <Person
           name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        >
-          {this.state.persons[0].job && <p> and my job is a {this.state.persons[0].job}</p>}
-        </Person>
+          age={this.state.persons[0].age} />
 
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          handlePersonName={this.handlePersonName} />
+          handlePersonName={this.handlePersonName.bind(this, 'bel7assen')}
+          onChangeName={this.onChangeName}/>
 
         <Person
           name={this.state.persons[2].name}
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Person = (props, refs) => {
-  console.log(refs);
+const Person = (props) => {
   return (
     <div>
       <p>My Name is {props.name} im {props.age} years old</p>
       {props.children}
-      <button onMouseMove={props.handlePersonName}>{props.name}</button>
+      <button onClick={props.handlePersonName}>{props.name}</button>
+      <input type="text" onChange={this.onChangeName} value={props} />
     </div>
   );
 }
