ae27778815e0dc60fe668346de763203167c81af
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-15 04:04:59 +0200

    play play :D

--- src/App.js
+++ src/App.js
@@ -2,28 +2,49 @@ import React, { Component } from 'react';
 import Person from './components/Person';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        {name: 'Belhassen', age: '21'},
+        {name: 'idiot', age: '22'},
+        {name: 'dump', age: '25'}
+      ]
+    }
+
+    this.handlePersonName = this.handlePersonName.bind(this);
+  }
+
+  handlePersonName() {
+    this.setState(() => ({
+      persons: [
+        {name: 'Belhassen', age: '21', job: 'developer'},
+        {name: 'idiot', age: '22'},
+        {name: 'dump', age: '25'}
+      ]
+    }));
+  }
+
+
   render() {
-    const names = ['Belhassen', 'Maher', 'Marwen'],
-    ages = [22, 30, 24];
     return (
       <div className="container">
         <h1>Play With ReactLove</h1>
-        <Person name={names[0]} age={ages[0]}>
-          <table border="1" style={{
-            borderCollapse: 'collapse'
-          }}>
-            <thead>
-              <tr>
-                <td>git</td>
-              </tr>
-              <tr>
-                <td>bel7aG</td>
-              </tr>
-            </thead>
-          </table>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          {this.state.persons[0].job && <p> and my job is a {this.state.persons[0].job}</p>}
         </Person>
-        <Person name={names[1]} age={ages[2]} />
-        <Person name={names[2]} age={ages[1]} />
+
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          handlePersonName={this.handlePersonName} />
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Person = (props) => (
-  <div>
-    <p>My Name is {props.name} im {props.age} years old</p>
-    {props.children}
-  </div>
-);
+const Person = (props, refs) => {
+  console.log(refs);
+  return (
+    <div>
+      <p>My Name is {props.name} im {props.age} years old</p>
+      {props.children}
+      <button onMouseMove={props.handlePersonName}>{props.name}</button>
+    </div>
+  );
+}
 
 export default Person;
