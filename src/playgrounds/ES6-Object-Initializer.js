2fa79b2cf22a876d51590a3c4b12ff19dc9375f0
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-23 08:52:11 +0200

    play with ES6 Object Initializer Playground play play play

---
+++ src/playgrounds/ES6-Object-Initializer.js
@@ -0,0 +1,28 @@
+// just play because more code more experience
+
+const myOne = {
+  a: function() {
+    console.log('holly holly');
+  },
+  ok: 'my okey',
+  myName: 'myName'
+};
+
+const correctMyOneUsingES6 = {
+  a() {
+    console.log('holly holly');
+  },
+  ok: 'my okey',
+  myName
+};
+
+// Last but not least use computed property names in JavaScript ES6
+
+const key =  'myName';
+const MyOneUsingES6Computed = {
+  a() {
+    console.log('holly holly');
+  },
+  ok: 'my okey',
+  [key]: 'bel7aG'
+};
