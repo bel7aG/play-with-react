3bd693d577af34e5fd2fdbc5da72cbc6599f862a
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-24 12:47:56 +0200

    play play play

--- src/playgrounds/DiveIntoTheRoadToLearnReactBook/EventHandler.js
+++ src/playgrounds/DiveIntoTheRoadToLearnReactBook/EventHandler.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const EventHandler = () => (
+const EventHandler = (props) => (
   <div className="road__event-handler">
     <h1>Event Handler</h1>
-
+    {props.list.length && (
+      props.list.map((item) => (
+        <div className={`item-${item.objectID}`} key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+    
+        </div>
+      ))
+    )}
   </div>
 );
 
--- src/playgrounds/DiveIntoTheRoadToLearnReactBook/RoadToLearnReact.js
+++ src/playgrounds/DiveIntoTheRoadToLearnReactBook/RoadToLearnReact.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import EventHandler from './EventHandler';
 
 export default class RoadToLearnReact extends Component {
+
+
+  state = {
+    list: [
+      {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Belhassen Gharsallah',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+      }, {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'bel7aG',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+      }
+    ]
+  };
+
+  onDismiss = (id) => {
+    this.setState((prevState) => ({
+      list: prevState.list.filter((item) => item.objectID !== id)
+    }));
+  }
+
   render() {
-    return (
-      <div className="road">
-        <EventHandler />
-      </div>
-    );
+
+
+    return (<div className="road">
+      <EventHandler list={this.state.list} />
+    </div>);
   }
 }
